9b320ba2dcf6544f657f9f645f79524976ea867d
RiccardoFiore <fiore.1705634@studenti.uniroma1.it>
2018-04-10 17:49:36 +0200

    definita SemClose

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,56 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+
+
+    int fd = running->syscall_args[0]; //prendo il file descriptor passato alla SemClose salvato nella variabile running in
+                                       //globals.h
+
+    printf("Sto chiudendo il semaforo con fd: %d\n", fd);
+
+    int ret;
+
+    //prendo il semaphore descriptor
+    SemDescriptor* sem_dsc= SemDescriptorList_byFd(&running->sem_descriptors, fd);
+    if(!sem_dsc){ //check per il semafore nel processo
+        running->syscall_retvalue = -111; //non ci sono processi con id = semnum nel sistema
+        return;
+    }
+
+    //prendo il semaforo corrispondente al sem_dsc
+    Semaphore* sem = sem_dsc->semaphore;
+
+    SemDescriptorPtr* sem_dsc_ptr = sem_dsc->ptr;
+
+    //elimino il sem_dsc dalla lista dei descrittori del processo chiamante
+    sem_dsc = (SemDescriptor*) List_detach(&(running->sem_descriptors), (ListItem*) sem_dsc);
+    ret = SemDescriptor_free(sem_dsc);
+    if(ret) {
+        running->syscall_retvalue = ret;
+        return;
+    }
+
+    //elimino il puntatore a sem_dsc dalla lista del semaforo con id = sem_num
+    sem_dsc_ptr = (SemDescriptorPtr*) List_detach(&(sem->descriptors), (ListItem*) sem_dsc_ptr);
+    ret = SemDescriptorPtr_free (sem_dsc_ptr);
+    if(ret) {
+        running->syscall_retvalue = ret;
+        return;
+    }
+
+
+    //eliminati tutti i descrittori eseguo la free di sem
+    if((sem->descriptors).size == 0){
+        sem = (Semaphore*) List_detach(&semaphores_list, (ListItem*) sem);
+        ret = Semaphore_free(sem);
+        if(ret != 0) {
+            running->syscall_retvalue = ret;
+            return;
+        }
+
+    }
+
+    running->syscall_retvalue = 0;
+
+
 }
